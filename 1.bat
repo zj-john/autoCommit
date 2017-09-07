70b4c325e2111ce3f939596b33910cee9040ebab
z_j <z_j@ctrip.com>
2017-09-07 18:34:46 +0800

    update

--- 1.bat
+++
@@ -1,1 +0,0 @@
-schtasks /create /tn "auto-commit" /tr "cmd /c node E:\Code\GitHub\autoCommit\index.js" /sc hourly /mo 8
--- task.js
+++ task.js
@@ -1,8 +1,7 @@
 const child_process = require("child_process");
 
 const execCMD = function(){
-  console.log('schtasks /create /tn "auto-commit" /tr "cmd /c node '+ __dirname + '\\index.js" /sc hourly /mo 8');
-  child_process.exec('schtasks /create /tn "auto-commit" /tr "cmd /c node '+ __dirname + '\\index.js" /sc hourly /mo 8', function(error, stdout, stderr) {
+  child_process.exec('schtasks /create /tn "auto-commit" /tr "cmd /c node '+ __dirname + '\\index.js" /sc hourly /mo 8 /f', function(error, stdout, stderr) {
     if(error) {
         console.error('error: ' + error);
         return;
