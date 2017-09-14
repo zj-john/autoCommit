645e1f977f066fdcf70ba448530622ba091a8f44
z_j <z_j@ctrip.com>
2017-09-14 11:07:18 +0800

    fixed hh to HH

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const crawlerMeta = new crawler({
   maxConnections: 1,
   callback: (error, res, done) => {
     if (error) {
-      utils.export_to_file(error, "./error/" + moment().format("YYYY-MM-DDThh-mm-ss") + '.txt');
+      utils.export_to_file(error, "./error/" + moment().format("YYYY-MM-DDTHH-mm-ss") + '.txt');
     } else {
       let $ = res.$;
       // 判断是否重定向, 如果页面数量超标的话则会重定向到其他页面
@@ -26,7 +26,7 @@ const crawlerMeta = new crawler({
             "desc": desc
           });
         });
-        utils.export_to_file(JSON.stringify(content), './file/' + moment().format("YYYY-MM-DDThh-mm-ss") + '.txt');
+        utils.export_to_file(JSON.stringify(content), './file/' + moment().format("YYYY-MM-DDTHH-mm-ss") + '.txt');
         // 需要先进入到代码所在的目录，再进行git操作
         const cmd = 'cd /D '+ __dirname + ' && git add --all && git commit -m "update" && git push origin master';
         utils.exec_cmd(cmd);
